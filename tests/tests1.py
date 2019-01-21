30eaa7a9b7dcda3e56400fcca2c0c6e9dfff2700
Sergey Bosko <s_bosko@apple.com>
2019-01-20 20:00:27 -0800

    added unit test for sub

--- tests/tests1.py
+++ tests/tests1.py
@@ -1,9 +1,13 @@
 import unittest
 
-from scripts.lesson1 import add
+from scripts.lesson1 import add, sub
 
 
 class MyFirstTest(unittest.TestCase):
     def test__add(self):
         result = add(8, 12)
         self.assertEqual(20, result)
+
+    def test__sub(self):
+        result = sub(12, 8)
+        self.assertEqual(4, result)
